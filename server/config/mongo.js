e7a509ba166cf58566aae965e416d65859d0b4e5
Sebastian Sossa <sebastian.sossa@educatic.com.co>
2022-03-23 22:56:54 -0500

    Environment

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect('mongodb://127.0.0.1:27017/crud-products', options)
+mongoose.connect(process.env.DB_URL, options)
   .then(() => console.log('Database is connected'))
   .catch(error => console.error(error))
